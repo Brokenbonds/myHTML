c943a9d79dffb774fa9d752a301bc1cf5bf389f5
Phillip Boswell <phillip.boswell@gmail.com>
2021-12-30 15:54:38 -0800

    VS Assignment
    Assignment Webpage made in Visual Studios

---
+++ Project 14 VS Website Assignment/AssignmentPage/AssignmentPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AssignmentPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
